53053490eccc22a2c75f7a7fff6a38fb2953f74e
[Dinma-Faith] <[phaytep@gmail.com]>
2022-08-11 12:11:29 +0100

    Add methods for like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,12 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
 
+  after_create :update_likes_counter
+
+  private
+
+  #   A method that updates the likes counter for a post.
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
