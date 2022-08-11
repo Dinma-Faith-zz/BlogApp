2363bd29d51eb73fc80d9bdd156984a203fdcd41
[Dinma-Faith] <[phaytep@gmail.com]>
2022-08-11 12:12:03 +0100

    Add method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
+  #   A method that returns the 3 most recent posts for a given user.
+  def posts_recent
+    posts.order(created_at: :desc).limit(3)
+  end
 end
