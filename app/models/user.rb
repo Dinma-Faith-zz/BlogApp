18743394ea43fe1d9fc78eb661d310346e48ac1c
[Dinma-Faith] <[phaytep@gmail.com]>
2022-08-11 16:36:14 +0100

    Validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
+  validates :name, presence: true
+  validates :postcounter, numericality: { greater_than_or_equal_to: 0 }
 
   #   A method that returns the 3 most recent posts for a given user.
   def posts_recent
