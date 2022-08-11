b6c9b7c8d0c0d89346166f8dc678defe41104800
[Dinma-Faith] <[phaytep@gmail.com]>
2022-08-11 12:10:59 +0100

    Add methods to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
 
+  after_create :update_comments_counter
+
+  private
+
+  #   A method that updates the comments counter for a post.
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
