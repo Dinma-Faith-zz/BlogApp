ecc01fb256f1d47fb2cab46752bed5e5e771702c
[Dinma-Faith] <[phaytep@gmail.com]>
2022-08-23 19:01:37 +0100

    Modify views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create]
